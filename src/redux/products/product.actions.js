faed04441e88bd5f11dd1b03ff1c698fd8cf9fcd
gokul nath <gokulaps1998@gmail.com>
2020-11-07 20:19:12 +0530

    abd

--- src/redux/products/product.actions.js
+++
@@ -1,8 +0,0 @@
-import { ProductActionTypes } from "./product.types";
-
-
-export const setHomePageProducts = homepageProducts =>({
-
-    type:ProductActionTypes.SET_HOMEPAGE_PRODUCTS,
-    payload:homepageProducts
-})
--- src/redux/products/product.types.js
+++
@@ -1,4 +0,0 @@
-export const ProductActionTypes = {
-    SET_HOMEPAGE_PRODUCTS: 'SET_HOMEPAGE_PRODUCTS'
-  };
-  
